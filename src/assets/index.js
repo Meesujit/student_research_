5f45b39d9ce6258562fa5ce824b77bd8fb76efab
sujit gouda <sujit.gda997@gmail.com>
2024-10-08 22:25:27 +0530

    home page update

--- src/assets/index.js
+++ src/assets/index.js
@@ -4,12 +4,13 @@ import location from './location.jpg';
 import heroImage from './heroimage.jpg'
 import logo from './logo.png'
 import relational from './collbrative.png'
-
+import achyutaSamantaImage from './founder.jpg';
 export  {
 SRSstudent,
 objective,
 location,
 heroImage,
 logo,
-relational
+relational,
+achyutaSamantaImage
 }
--- src/components/Founder.jsx
+++ src/components/Founder.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React from 'react';
+import { achyutaSamantaImage } from '../assets/index.js';
 
 const Founder = () => {
-  return (
-    <div>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsam corrupti odio eius dolore autem accusantium delectus minus itaque sint corporis reprehenderit, excepturi consequuntur omnis iusto quis, rerum maiores aspernatur pariatur!
-    </div>
-  )
-}
+    return (
+        <div className="founder-section">
+            <h2 className="founder-heading">Founder & Mentor</h2>
+            <div className="founder-container">
+                <img src={achyutaSamantaImage} alt="Achyuta Samanta" className="founder-image" />
+                <div className="founder-content">
+                    <h2>Achyuta Samanta</h2>
+                    <p>
+                        Achyuta Samanta is the founder of KIIT (Kalinga Institute of Industrial Technology) and KISS (Kalinga Institute of Social Sciences).
+                        He is known for his contributions to education and social service in India, and his efforts have transformed the lives of many underprivileged children through education.
+                    </p>
+                    <p>
+                        With a vision to make quality education accessible to all, Achyuta Samanta has established various initiatives aimed at empowering students and fostering a culture of research and innovation.
+                    </p>
+                </div>
+            </div>
+        </div>
+    );
+};
 
-export default Founder
+export default Founder;

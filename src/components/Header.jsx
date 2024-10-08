e52feeb720b96124b361afa169d3f13aea5ea810
sujit gouda <sujit.gda997@gmail.com>
2024-10-08 20:19:26 +0530

    header update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import logo from '../assets/KIIT-FULL-LOGO-LEFT.png';
+import { logo } from '../assets';
 
 function Header() {
   return (
